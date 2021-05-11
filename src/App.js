65afeea81e56dc72ad71ff6435428eaac9bcb5b1
Sabia Tahir <sabiatahir@hotmail.com>
2021-05-11 19:12:53 -0400

    create function to fetch movie info

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const fetchData = (query) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB}&language=en-US&query=${query}&page=1&include_adult=false`
+      )
+      .then((response) => console.log(response.data.results));
+  };
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <SearchBar />
+      <SearchBar fetchData={fetchData} />
     </div>
   );
 }
