1d6427d93ed642d7ce2ade510fbe93f3e23ce1df
Sabia Tahir <sabiatahir@hotmail.com>
2021-05-11 19:14:47 -0400

    make search bar controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 
 export default function SearchBar(props) {
   const styling = { width: "50rem", padding: "0.5rem" };
+  const [input, setInput] = useState("");
   return (
     <>
-      <input type="text" style={styling}></input>
-      <button>Search</button>
+      <input
+        type="text"
+        style={styling}
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      ></input>
+      <button onClick={() => props.fetchData(input)}>Search</button>
     </>
   );
 }
