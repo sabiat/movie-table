3a88c1f6befcbd9edd3a7f59a13f9ef31e0c0dc8
Sabia Tahir <sabiatahir@hotmail.com>
2021-05-12 18:28:40 -0400

    Add vote to table

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,19 +4,23 @@ import "react-tabulator/lib/styles.css";
 
 export default function Results(props) {
   const columns = [
-    { title: "Name", field: "title", width: 150 },
+    { title: "Name", field: "title" },
     {
       title: "Release Date",
       field: "release_date",
       hozAlign: "left",
-      width: 150,
     },
     {
-      title: "Description",
-      field: "overview",
-      formatter: "textarea",
+      title: "Average Vote",
+      field: "vote_average",
     },
-    { title: "Image", field: "rating", hozAlign: "center", formatter: "star" },
+    // {
+    //   title: "Description",
+    //   field: "overview",
+    //   formatter: "textarea",
+    //   height: 150,
+    // },
+    // { title: "Image", field: "rating", hozAlign: "center", formatter: "star" },
     {
       title: "Popularity",
       field: "popularity",
@@ -31,13 +35,19 @@ export default function Results(props) {
   };
   return (
     <div className="Results">
-      <ReactTabulator
-        data={data}
-        columns={columns}
-        tooltips={true}
-        layout={"fitColumns"}
-        height={320}
-      />
+      {data.length > 0 && (
+        <>
+          <h3>Search Results:</h3>
+          <ReactTabulator
+            data={data}
+            columns={columns}
+            tooltips={true}
+            layout={"fitData"}
+            height={420}
+            selectable={true}
+          />
+        </>
+      )}
     </div>
   );
 }
