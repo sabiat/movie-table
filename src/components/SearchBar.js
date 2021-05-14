186ea312fc812f3d7675cd4acfddaedcba09e3e9
Sabia Tahir <sabiatahir@hotmail.com>
2021-05-13 22:17:47 -0400

    Can search on enter now

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,6 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function SearchBar(props) {
   const styling = { width: "40%", padding: "0.5rem" };
   const [input, setInput] = useState("");
+
+  const handleKeyPress = function (event) {
+    if (event.key === "Enter") {
+      props.fetchData(input);
+    }
+  };
+
   return (
     <div className="SearchContainer">
       <input
@@ -13,6 +20,7 @@ export default function SearchBar(props) {
         placeholder="Ex. 'Batman'"
         value={input}
         onChange={(event) => setInput(event.target.value)}
+        onKeyUp={(event) => handleKeyPress(event)}
       ></input>
       <Button onClick={() => props.fetchData(input)}>Search</Button>
     </div>
